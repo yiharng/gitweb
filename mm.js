a00eda5a1442e60af6eb32c21fc5b0c0882f08c7
yiharng <yiharng@gmail.com>
2020-06-20 23:12:00 +0800

    zoom

--- mm.js
+++ mm.js
@@ -114,10 +114,10 @@ $(function()
 
         let addbtn=""
         +"<button onclick='$(this).parent().parent().css(\"font-size\","
-        +"parseInt($(this).parent().parent().css(\"font-size\"))*1.1+\"px\""
+        +"parseFloat($(this).parent().parent().css(\"font-size\"))*1.1+\"px\""
         +")' style='width:3em;font-size:1em;'>+</button>&nbsp;&nbsp;"
         +"<button onclick='$(this).parent().parent().css(\"font-size\","
-        +"parseInt($(this).parent().parent().css(\"font-size\"))*0.9+\"px\""
+        +"parseFloat($(this).parent().parent().css(\"font-size\"))*0.9+\"px\""
         +")' style='width:3em;font-size:1em;'>-</button><br><br>";
 
         if (txt.attr("zoom"))
